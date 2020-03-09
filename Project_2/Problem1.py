32768fac8c815e728016cb80efdfd1fb03a9fe3e
SrikumarMuralidharan <smuralid@umd.edu>
2020-03-09 17:44:06 -0400

    Small edits to problem 1

--- Project_2/Problem1.py
+++ Project_2/Problem1.py
@@ -1,6 +1,8 @@
+## Contrast limited adaptive Histogram Equalisation CLAHE
+
 import numpy as np
 import cv2
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 i=0
 video = cv2.VideoCapture('Night Drive - 2689.mp4')
@@ -11,13 +13,15 @@ while (True):
         image = cv2.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
         image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         h,s,v = cv2.split(image_hsv.copy())
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(10,10))
         output_h = clahe.apply(h)
         output_s = clahe.apply(s)
         output_v = clahe.apply(v)
         output = cv2.merge((output_h, output_s, output_v))
         fin_output = cv2.cvtColor(output, cv2.COLOR_HSV2BGR)
-        cv2.imshow('output', output)
+        fin_output = cv2.fastNlMeansDenoisingColored(fin_output, None, 10, 10, 5, 15)
+        #fin_output = cv2.medianBlur(fin_output, ksize=3)
+        #cv2.imshow('output', output)
         cv2.imshow('output2', fin_output)
         cv2.imshow('image', image)
         if cv2.waitKey(2) & 0xff == ord('q'):
